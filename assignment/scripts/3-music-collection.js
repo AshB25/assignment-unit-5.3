7071d0479c262810eda4c66d7ff93ecf8ec44938
Ashleigh Carter <ashbcarter@gmail.com>
2023-11-25 20:48:05 -0500

    my collection array & function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,11 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+myCollection = [];
+function addToCollection(Collection, title, artist, yearPublished){
+
+}
+
 
 
 
